5e53afc447d9e4eca1f5141946c6d8b0a24b02c7
apolIinaria <pnovomlynets@gmail.com>
2023-11-28 20:12:06 +0200

    Добавьте файлы проекта.

---
+++ Task_1-2.1/Task_1-2.1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+vector<vector<int>> createMatrix(int n) 
+{
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix[i][j] = rand() % 101 - 50;
+        }
+    }
+
+    return matrix;
+}
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int value : row) {
+            cout << setw(4) << value;
+        }
+        cout << endl;
+    }
+}
+
+vector<vector<int>> rotate(const vector<vector<int>>& matrix) 
+{
+    int n = matrix.size();
+    vector<vector<int>> rotatedMatrix(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            rotatedMatrix[i][j] = matrix[n - j - 1][i];
+        }
+    }
+    return rotatedMatrix;
+}
+
+int main() 
+{
+    srand(time(nullptr));
+
+    int n;
+    cout << "Введіть розмірність матриці n: ";
+    cin >> n;
+
+    vector<vector<int>> matrix = createMatrix(n);
+
+    cout << "Початкова матриця:" << endl;
+    printMatrix(matrix);
+
+    vector<vector<int>> rotatedMatrix = rotate(matrix);
+
+    cout << "Матриця після розвертання на 90 градусів за часовою стрілкою:" << endl;
+    printMatrix(rotatedMatrix);
+
+    return 0;
+}
---
+++ Task_2.6/Task_3.6.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std;
+
+int calculate_sum(const vector<vector<int>>& matrix) {
+    int sum = 0;
+    for (const auto& row : matrix) {
+        for (int element : row) {
+            if (element > 0) {
+                sum += element;
+            }
+        }
+    }
+    return sum;
+}
+
+void calculate_count(const vector<vector<int>>& matrix, int Z) {
+    int columns = matrix[0].size();
+    for (int col = 0; col < columns; ++col) {
+        int count = 0;
+        for (const auto& row : matrix) {
+            if (row[col] <= Z) {
+                count++;
+            }
+        }
+        cout << "Стовпець " << col + 1 << ": " << count << " елементів не перевищують " << Z << endl;
+    }
+}
+
+void fill_Matrix(vector<vector<int>>& matrix) {
+    srand(time(0));
+    int size = matrix.size();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            matrix[i][j] = rand() % 100 + 1 ;
+        }
+    }
+}
+
+vector<vector<int>> transpose_Matrix(const vector<vector<int>>& matrix) {
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    vector<vector<int>> result(columns, vector<int>(rows));
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+
+    return result;
+}
+
+vector<vector<int>> addMatrices(const vector<vector<int>>& matrix1, const vector<vector<int>>& matrix2) {
+    int rows = matrix1.size();
+    int columns = matrix1[0].size();
+    vector<vector<int>> result(rows, vector<int>(columns));
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    return result;
+}
+
+void print_Matrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int element : row) {
+            cout << element << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main() 
+{
+    const int size = 5;
+
+    vector<vector<int>> matrix(size, vector<int>(size));
+    vector<vector<int>> transposedMatrix(size, vector<int>(size));
+
+    fill_Matrix(matrix);
+    cout << "Вихідна матриця:" << endl;
+    print_Matrix(matrix);
+
+    int sum = calculate_sum(matrix);
+    cout << "Сума додатних елементів: " << sum << endl;
+
+    int Z;
+    cout << "Введіть число Z: " << endl;
+    cin >> Z;
+    calculate_count(matrix, Z);
+
+    transposedMatrix = transpose_Matrix(matrix);
+    cout << "Транспонована матриця:" << endl;
+    print_Matrix(transposedMatrix);
+
+    vector<vector<int>> sumMatrix = addMatrices(matrix, transposedMatrix);
+    cout << "Сума матриці та її транспонованої:" << endl;
+    print_Matrix(sumMatrix);
+
+    return 0;
+}
+
